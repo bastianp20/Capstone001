cef6200847b292aa50ce4963056af1b69474086f
Bastian Andres Palavecino Ancalaf <ba.palavecino@duocuc.cl>
2026-08-25 23:04:15 -0400

    primeros cambios en la app web, solo visualización junto con los datos falsos de prueba.

---
+++ Patuki/src/components/MockupBadge/MockupBadge.tsx
@@ -0,0 +1,7 @@
+import './MockupBadge.css'
+
+function MockupBadge() {
+  return <div className="badge-note">Mockup visual · no funcional</div>
+}
+
+export default MockupBadge
---
+++ Patuki/src/components/Partners/Partners.tsx
@@ -0,0 +1,17 @@
+import './Partners.css'
+
+function Partners() {
+  return (
+    <div className="logos">
+      <div className="wrap logos-inner">
+        <span className="logos-label">Pensado para trabajar junto a</span>
+        <span className="logo-chip">Clínicas Veterinarias</span>
+        <span className="logo-chip">Refugios & Rescates</span>
+        <span className="logo-chip">Municipalidades</span>
+        <span className="logo-chip">Fundaciones Animalistas</span>
+      </div>
+    </div>
+  )
+}
+
+export default Partners
